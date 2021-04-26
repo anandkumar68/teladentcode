dd4184f23c265b7563ef949a112299236cb38dae
anandks <anand6824@gmail.com>
2021-04-26 11:21:13 +0530

    fist commit new

---
+++ src/app/module/oral-self-asessment/oral-self-asessment.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-oral-self-asessment',
+  templateUrl: './oral-self-asessment.component.html',
+  styleUrls: ['./oral-self-asessment.component.css']
+})
+export class OralSelfAsessmentComponent implements OnInit {
+  
+
+   step: any = 1;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+
+}
+
